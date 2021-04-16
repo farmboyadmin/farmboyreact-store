244f140292e3460710270a02424fa583d886ede9
dsam1 <david.sam@optum.com>
2021-04-16 12:55:26 -0500

    added logo

---
+++ pages/aboutus.vue
@@ -0,0 +1,27 @@
+<template>
+      <h2>Hope Farm School is a boarding school near Stockholm, Wisconsin with the mission of discipling, training, and educating young men in the spirit of Jesus Christ, all in a farm setting.
+           Through Farmboy Industries we teach our students business principles, and make the learning real by selling our products. Proceeds support our school.</h2>
+      <p>
+      For more details
+      <a href="https://www.hopefarmschool.org">Visit hopefarmschool.com!</a>
+      </p>
+</template>
+
+<script>
+
+
+export default {
+};
+</script>
+
+<style>
+
+  body {
+    font-family: sans-serif;
+  }
+  label {
+    display: block;
+    margin-bottom: .2em;
+  }
+
+</style>
