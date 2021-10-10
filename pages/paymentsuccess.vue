cc47964baa7c9ff2c6d68d215d394ef182e73265
dsam1 <david.sam@optum.com>
2021-10-10 17:49:55 -0500

    commit

--- pages/paymentsuccess.vue
+++ pages/paymentsuccess.vue
@@ -1,7 +1,8 @@
 <template>
   <section>
-     <h1>Purchase successful! Order reciept sent to the email provided during checkout</h1>
-      <p>Thank you for supporting Hope Farm School. Please reach out to Widdy at 612-710-2868 if you have any questions or concerns.</p>
+     <p>Purchase successful! Order reciept sent to the email provided during checkout.</p>
+      <p>You will recieve an email about the delivery date of the meat.</p>
+      <p>Thank you for supporting Hope Farm School. Please reach out to Widdy at 612-710-2868 if you have any questions or to arranger a time if you are picking up your meat.</p>
   </section>
 </template>
 
